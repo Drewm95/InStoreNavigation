7e0c9914cc3b20eb8f649c21cbeba12325254bfb
Andrew <drewmolloy2@gmail.com>
2017-10-03 16:04:45 -0400

    Added some Magentic field sensor code

--- app/src/main/java/com/example/andrew/petometertesting/MainActivity.java
+++ app/src/main/java/com/example/andrew/petometertesting/MainActivity.java
@@ -12,6 +12,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -22,11 +23,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     //declare the sensor manager
     private SensorManager sensorManager;
-    private Sensor mAccel;
+    private Sensor mAccel,mMag;
     private int stepCount, stepSense;
     private Date lastUpdate;
-    private float lastZ, newZ, avgZ, count,zSum, newX, newY, lastx, lasty;
-    private TextView stepDetected,lowAccuracyWarning,x,y,z,avgZText, stepCountView;
+    private float lastZ, newZ, avgZ, count,zSum, newX, newY, lastx, lasty, mag1Value, mag2Value, mag3Value;
+    private TextView stepDetected,lowAccuracyWarning,x,y,z,avgZText, stepCountView,mag1,mag2, mag3;
+    private ProgressBar pbar1, pbar2, pbar3;
 
     /*
 
@@ -50,7 +52,9 @@ public class MainActivity extends Activity implements SensorEventListener {
         z = (TextView)findViewById(R.id.zText);
         avgZText = (TextView)findViewById(R.id.zAverage);
         stepCountView = (TextView)findViewById(R.id.stepCount);
-
+        pbar1 = (ProgressBar) findViewById(R.id.progressBar4);
+        pbar2 = (ProgressBar) findViewById(R.id.progressBar6);
+        pbar3 = (ProgressBar) findViewById(R.id.progressBar7);
 
         //get the reset button reference
         final Button button = (Button) findViewById(R.id.button);
@@ -88,21 +92,31 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         //get the default accelerometer from the sm
         if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-
-            mAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
             //there is a accelerameter
-            lastZ = 0; //TODO do this better
-            lastx= 0;
-            lasty = 0;
-            zSum = 0;
-            avgZ = 0;
-            stepSense = 3;
+            mAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         } else {
 
         }
 
+        //get the default magnetometer from the sm
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+            //there is a magnetometer
+            mMag = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+            //DEBUG TEXT
+            //mag1.setText("SENSOR FOUND");
+        } else {
+           // mag1.setText("SENSOR NOT PRESENT");
+        }
+
+        //set all of the required initial last acceleration values
+        lastZ = 0; //TODO do this better
+        lastx= 0;
+        lasty = 0;
+        zSum = 0;
+        avgZ = 0;
+        stepSense = 3;
         //get the time, this is used to prevent counting a single step multiple times
         lastUpdate = Calendar.getInstance().getTime();
 
@@ -113,7 +127,8 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     protected void onResume(){
         super.onResume();
-        sensorManager.registerListener(this, mAccel , SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, mAccel , SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, mMag, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -124,46 +139,61 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        //get the latest value from the sensor
+        if(event.sensor == mAccel) {
 
-        newX = event.values[0];
-        newY = event.values[1];
-        newZ = event.values[2];
-        //check if the new value is much different than the last one
-        if(newZ + newX + newY > lastZ + lastx +lasty + stepSense) {
+            //get the latest value from the sensor
 
-            //make sure it has been at least 10 milliseconds since the last step count
-            //if not don't count it, people don't walk that fast
-            if(Calendar.getInstance().getTimeInMillis() > lastUpdate.getTime() + 500) {
-                //A step was detected
-                stepDetected.setVisibility(View.VISIBLE);
-                stepCount++;
-                stepCountView.setText(Integer.toString(stepCount));
-                lastUpdate = Calendar.getInstance().getTime();
+            newX = event.values[0];
+            newY = event.values[1];
+            newZ = event.values[2];
+            //check if the new value is much different than the last one
+            if (newZ + newX + newY > lastZ + lastx + lasty + stepSense) {
+
+                //make sure it has been at least 10 milliseconds since the last step count
+                //if not don't count it, people don't walk that fast
+                if (Calendar.getInstance().getTimeInMillis() > lastUpdate.getTime() + 500) {
+                    //A step was detected
+                    stepDetected.setVisibility(View.VISIBLE);
+                    stepCount++;
+                    stepCountView.setText(Integer.toString(stepCount));
+                    lastUpdate = Calendar.getInstance().getTime();
+                }
+            } else {
+                stepDetected.setVisibility(View.INVISIBLE);
             }
+
+            //report other readings
+            if (newX > lastx + 0.1 || newX < lastx - 0.1)
+                x.setText(Float.toString(event.values[0]));
+            if (newY > lasty + 0.1 || newY < lasty - 0.1)
+                y.setText(Float.toString(event.values[1]));
+            if (newZ > lastZ + 0.1 || newZ < lastZ - 0.1)
+                z.setText(Float.toString(event.values[2]));
+
+            //get the average z
+            count++;
+            zSum += newZ;
+            avgZ = zSum / count;
+            avgZText.setText(Float.toString(avgZ));
+
+            //assign value to lastxyz before moving on
+            lastZ = newZ;
+            lastx = newX;
+            lasty = newY;
         }
-        else {
-            stepDetected.setVisibility(View.INVISIBLE);
+        if (event.sensor == mMag) {
+
+            //get the magnetic field value
+            mag1Value = event.values[0];
+            mag2Value = event.values[1];
+            mag3Value = event.values[2];
+
+
+            //set the textview to the new value\
+            pbar1.setProgress((int)mag1Value);
+            pbar2.setProgress((int)mag2Value);
+            pbar3.setProgress((int)mag3Value);
         }
-
-        //report other readings
-        if(newX > lastx + 0.1 || newX < lastx - 0.1)
-        x.setText(Float.toString(event.values[0]));
-        if(newY > lasty+ 0.1 || newY < lasty - 0.1)
-        y.setText(Float.toString(event.values[1]));
-        if(newZ > lastZ + 0.1 || newZ < lastZ - 0.1)
-        z.setText(Float.toString(event.values[2]));
-
-        //get the average z
-        count++;
-        zSum +=  newZ;
-        avgZ = zSum / count;
-        avgZText.setText(Float.toString(avgZ));
-
-        //assign value to lastxyz before moving on
-        lastZ = newZ;
-        lastx = newX;
-        lasty = newY;
     }
 
     @Override
